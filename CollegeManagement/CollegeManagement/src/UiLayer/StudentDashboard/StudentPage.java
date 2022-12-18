41c943b2e8eae4d6cefb7544b861e0bea495b820
VENKATESAN J <101502792+Venkatesan15@users.noreply.github.com>
2022-12-18 17:12:08 +0530

    Delete CollegeManagement/CollegeManagement directory

--- CollegeManagement/CollegeManagement/src/UiLayer/StudentDashboard/StudentPage.java
+++
@@ -1,8 +0,0 @@
-package UiLayer.StudentDashboard;
-
-public interface StudentPage {
-    void feesDetails(String rollNoOrName);
-    void sendQuery(String rollNoOrName);
-    void studentProfile(String rollNoOrName);
-    void showTimeTable();
-}
